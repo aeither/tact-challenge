b36c75dad92201fa355bb3ea4795ab2652675da4
aeither <36173828+aeither@users.noreply.github.com>
2023-10-16 17:35:11 +0300

    test1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -30,7 +30,13 @@ describe('Task1', () => {
     });
 
     it('test', async () => {
+        const deployer = await blockchain.treasury('deployer');
+        await task1.send(
+            deployer.getSender(),
+            { value: toNano(0) },
+            { $$type: 'Add', queryId: BigInt(1), number: BigInt(123) }
+        );
         const count = await task1.getCounter();
-        console.log(count);
+        console.log(BigInt(count).toString());
     });
 });

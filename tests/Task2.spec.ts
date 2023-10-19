db3cccc97de1c433d205efa804d43987cb33f0fc
aeither <36173828+aeither@users.noreply.github.com>
2023-10-19 13:48:35 +0300

    clean

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -47,7 +47,7 @@ describe('Task2', () => {
                 .storeAddress(user.address)
                 .storeRef(beginCell().storeSlice(beginCell().storeInt(78, 16).endCell().asSlice()).endCell())
                 .endCell(),
-            value: (x) => (x ? toNano('0.00') <= x && x <= toNano('0.05') : false),
+            value: (x) => (x ? toNano('0.00') < x && x <= toNano('0.05') : false),
         });
     });
 
@@ -73,39 +73,7 @@ describe('Task2', () => {
             from: task2.address,
             to: user.address,
             success: true,
-            value: (x) => (x ? toNano('0.00') <= x && x <= toNano('0.05') : false),
+            value: (x) => (x ? toNano('0.00') < x && x <= toNano('0.05') : false),
         });
     });
-
-    // it('should forward from another wallet', async () => {
-    //     const deployer = await blockchain.treasury('deployer');
-    //     let user = await blockchain.treasury('user');
-    // const result = await user.send({
-    //     to: task2.address,
-    //     value: toNano('1'),
-    //     body: beginCell().storeAddress(context().sender).storeRef(beginCell().storeSlice(msg).endCell()).endCell(),
-    // });
-    // expect(result.transactions).toHaveTransaction({
-    //     from: task2.address,
-    //     to: deployer.address,
-    //     body: beginCell()
-    //         .storeAddress(user.address)
-    //         .storeRef(beginCell().storeStringTail('Hello, world!').endCell())
-    //         .endCell(),
-    //     value: (x) => (x ? toNano('0.99') <= x && x <= toNano('1') : false),
-    // });
-    // });
 });
-
-/*
-  TASK 2 - Proxy 
-  Create a contract that forwards all received TONs
-  to the admin contract (whose address is set in init_store).
-  Message from this proxy contract to the admin contract should contain:
-    - Address of user who sent original message (should be stored in the outcoming body's data/bits)
-    - Original message that proxy smart contract received from user (should be stored in the outcoming body's first ref)
-  Also, if admin contract decides to reject message (if it sends to the proxy "Refund" message with opcode=0x44),
-  proxy contract needs to forward all TONs (attached to Refund message) back to the user.
-  User address will be provided in Refund message body as "sender".
-  In refund transaction, it is important to have a check that the refund message came from the admin address
-*/
